aba4e5a6fd9b30c196139080410702d1c9d9ef55
Jenn Lao <jenn.p.lao@gmail.com>
2019-02-23 13:37:52 -0700

    Refactor WebCal service

--- app/services/web_cal_service.rb
+++ app/services/web_cal_service.rb
@@ -1,8 +1,15 @@
 class WebCalService
 
   def get_holidays
-    conn = Faraday.new()
-    response = conn.get("http://www.webcal.fi/cal.php?id=50&format=json&start_year=current_year&end_year=next_year&tz=America%2FDenver")
-    json = JSON.parse(response.body, symbolize_names: true)
+    to_json("http://www.webcal.fi/cal.php?id=50&format=json&start_year=current_year&end_year=next_year&tz=America%2FDenver")
+  end
+
+  private
+  def conn
+    Faraday.new()
+  end
+
+  def to_json(url)
+    JSON.parse(conn.get(url).body, symbolize_names: true)
   end
 end
